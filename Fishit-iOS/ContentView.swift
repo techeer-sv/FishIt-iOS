380bb58b2ce929336b0caddd154f6397e2b4564a
POL6463 <kevinkim0617kim@gmail.com>
2022-08-26 22:46:44 -0700

    Initial Commit

---
+++ Fishit-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fishit-iOS
+//
+//  Created by Minwoong Kim on 2022/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fishit-iOS/Fishit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fishit_iOSApp.swift
+//  Fishit-iOS
+//
+//  Created by Minwoong Kim on 2022/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Fishit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
