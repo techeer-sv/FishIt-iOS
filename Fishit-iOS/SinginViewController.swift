76a7c91e3410b500441c7b76ab8132b8971e9c78
Somi <rlathal009@gmail.com>
2022-09-01 16:42:04 +0900

    feat : create sendPostRequest function

---
+++ Fishit-iOS/SinginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUp.swift
+//  Fishit-iOS
+//
+//  Created by Somi Kim on 2022/09/01.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
