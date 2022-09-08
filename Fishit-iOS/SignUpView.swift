97439d0dc6ba84dac814a1d9ff48d3d83528cac8
Somi <rlathal009@gmail.com>
2022-09-09 01:52:17 +0900

    feat : add post function in signup button

--- Fishit-iOS/SignUpView.swift
+++ Fishit-iOS/SignUpView.swift
@@ -41,14 +41,24 @@ struct SignUpView: View {
             	Text("비밀번호가 일치하지 않습니다.").foregroundColor(.red).font(.system(size: 10))
             }
             Spacer()
-            Button(action: {print("ButtonPressed")}){
+            Button(action: {
+                print("ButtonPressed")
+                sendPostRequest("http://3.39.75.19:8080/api/v1/users/new", parameters:
+                    ["email": self.email,
+                     "memberName": self.username,
+                     "password": self.password
+                     ]){
+                    responseObject, error in guard let _ = responseObject, error == nil else {
+                        print(error ?? "Unknown error")
+                        return
+                    }
+                }
+            }){
             	Text("회원가입").tint(.white)
             }.frame(width: 345, height: 47, alignment: .center)
             .background(Color("MainColor2"))
             .cornerRadius(8)
             .shadow(color: .gray, radius: 5, x: 3, y: 3)
-            
-            	
         }
     }
 }
